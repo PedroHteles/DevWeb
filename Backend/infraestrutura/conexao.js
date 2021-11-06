a8f4e0b13d1b379d5398930f8f55d6a8c92c6b3e
PedroHteles <74112798+PedroHteles@users.noreply.github.com>
2021-11-06 10:03:41 -0300

    final

--- Backend/infraestrutura/conexao.js
+++ Backend/infraestrutura/conexao.js
@@ -1,10 +1,13 @@
-const Sequelize = require('sequelize');
+const mysql = require('mysql')
 
-const connection = new Sequelize('compreArcom','root','password',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const conexao = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'comprearcom'
+})
 
-module.exports = connection;
+module.exports = conexao
 
 //roda issaq no mysql se n n roda ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123'; 
